1e49dfcd529862c90e8efd2fb3ee69d6f32c81d2
hengmhs <31433170+hengmhs@users.noreply.github.com>
2021-06-17 22:00:21 +0800

    Add GET for new form route

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Messageboard' , messages: messages});
 });
 
+/* new form */
+router.get('/new', function(req, res, next) {
+  res.render('form', {title: 'Mini Messageboard'});
+});
+
 module.exports = router;
